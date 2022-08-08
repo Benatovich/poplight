164ea1b7f37e90dd7a59f9810caf6369053d4810
Benatovich <sambenatovich@gmail.com>
2022-08-08 02:38:18 -0400

    code copied over from old repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,20 @@
-export default function App() {
-  return <h1>Hello World</h1>;
+import React, { useContext } from 'react'
+import './App.css'
+
+import Stoplight from './components/Stoplight'
+import Header from './components/Header'
+import Container from './components/Container'
+import { Context } from './context/Context'
+
+function App() {
+  const {darkMode} = useContext(Context);
+
+    return (
+        <div className={darkMode ? 'app dark' : 'app light'}>
+          <Header />
+          <Container />
+        </div>
+    )
 }
+
+export default App
